46fb1c2fbd2f80c12dd18c02406ca4171cf6f4db
Sebastian Fix <sebastian.fix@codebar.ch>
2021-06-04 23:03:37 +0200

    WIP

---
+++ resources/lang/en/application/team.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+
+    'title' => 'Team',
+    'subtitle' => 'The website as well as the self-assessment tool have been developed as part of a student project for the course in Cyber Security and Resilience. The project team consists of the following students',
+];
